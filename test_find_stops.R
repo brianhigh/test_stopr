36b6df074ee555f5ac96b699588054ea40a21af3
Brian High <brianhigh@users.noreply.github.com>
2023-10-10 14:21:45 -0700

    Updated URL in test_find_stops.R

--- test_find_stops.R
+++ test_find_stops.R
@@ -3,7 +3,7 @@
 # Filename: test_find_stops.R
 # Copyright (c) Brian High
 # License: MIT https://opensource.org/licenses/MIT (See LICENSE file.)
-# Repository: https://github.com/brianhigh/stopr
+# Repository: https://github.com/brianhigh/test_stopr
 
 # ----- Functions ------
 
