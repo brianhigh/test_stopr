ef868803cb4aed5b8069553ed6b12457ba63a596
Brian High <brianhigh@users.noreply.github.com>
2023-10-10 14:20:28 -0700

    Minor clode cleanup in import_test_tack_gpx.R

--- import_test_tack_gpx.R
+++ import_test_tack_gpx.R
@@ -1,5 +1,4 @@
 # Import the test GPS track data in GPX format and save as CSV.
-# Note: The GPX file was exported from Geo Tracker version 4.0.2.1750 (Android).
 
 # Filename: import_test_track_gpx.R
 # Copyright (c) Brian High
@@ -10,12 +9,15 @@
 if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
 pacman::p_load(here, tibble, plotKML, dplyr, lubridate, readr)
 
-# Import GPX file.
+# Define variables
 gpx_path <- here("2022-01-01_Morning_Run.gpx")
+csv_path <- here("test_data.csv")
+
+# Import GPX file.
 df <- as_tibble(readGPX(gpx_path)$tracks[[1]][[1]]) %>% 
   select(-ele) %>% 
   rename(longitude = lon, latitude = lat, datetime = time) %>% 
   mutate(datetime = as_datetime(datetime))
 
 # Save as CSV.
-write_csv(df, here("test_data.csv"))
+write_csv(df, csv_path)
